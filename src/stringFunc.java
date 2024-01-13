5048436b9710b876e8e4e1e5083eea6397a51146
naimur <naimurrahman18thapril@gmail.com>
2024-01-14 02:30:31 +0600

    first commit

---
+++ src/stringFunc.java
@@ -0,0 +1,7 @@
+public class stringFunc {
+    public static void main (String[] args){
+        System.out.println("Setup done");
+        String name ="Emon";
+        System.out.println(name.toUpperCase());
+    }
+}
