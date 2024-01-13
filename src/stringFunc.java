dbf659bc8c2eb49050532f05ce183dae42bddc64
naimur <naimurrahman18thapril@gmail.com>
2024-01-14 02:39:31 +0600

    second push

--- src/stringFunc.java
+++ src/stringFunc.java
@@ -3,5 +3,9 @@ public class stringFunc {
         System.out.println("Setup done");
         String name ="Emon";
         System.out.println(name.toUpperCase());
+String[] foods={"Pasta","Pizza","Coke"};
+        for (int i = 0; i < foods.length; i++) {
+            System.out.println(i+1+". "+foods[i]);
+        }
     }
 }
